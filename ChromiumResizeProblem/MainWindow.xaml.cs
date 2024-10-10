76ddc44d038c4a9d963d173c6f1646d42e4f5df1
Andreas Oetjen <oetjen@belox.de>
2024-10-10 12:17:40 +0200

    initial commit

---
+++ ChromiumResizeProblem/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChromiumResizeProblem
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string curDir = Directory.GetCurrentDirectory();
+            var url = string.Format("file:///{0}/page.html", curDir);
+            ChromiumWebBrowser[] browsers = { Browser1, Browser2, Browser3, Browser4, Browser5 };
+            foreach (var browser in browsers)
+            {
+                browser.LoadUrl(url);
+            }
+
+        }
+    }
+}
